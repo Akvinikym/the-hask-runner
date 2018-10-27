ee765d7f8c2a8ed201d3ac4196b6e67c7b6f798c
Yuriy Gavrilin <ygavrilin@provectus.com>
2018-10-27 20:50:22 +0300

    fix intersectTrapBounds

--- game/HaskRunner/Generation/Generator.hs
+++ game/HaskRunner/Generation/Generator.hs
@@ -7,56 +7,56 @@ import HaskRunner.Core
 
 type Seed = Double
 
-{-|
-Generate infinite list if objects from random seed
-Example use in drawing: 
-    drawGame :: Game -> Picture
-    drawGame = drawObjects . objectGenerator
--}
-objectGenerator :: Seed -> [GameObject]
-objectGenerator _ = startObjects ++ []
 
--- Objects at the start of the level
-startObjects :: [GameObject]
-startObjects = []
+-- {-|
+-- Generate infinite list if objects from random seed
+-- Example use in drawing: 
+--     drawGame :: Game -> Picture
+--     drawGame = drawObjects . objectGenerator
+-- -}
+-- objectGenerator :: Seed -> [GameObject]
+-- objectGenerator _ = startObjects ++ []
 
--- Infinite list of gameObj batches
-levelGenerator :: Seed -> [[GameObject]]
-levelGenerator s = _
+-- -- Objects at the start of the level
+-- startObjects :: [GameObject]
+-- startObjects = []
 
--- Randomly generate walls
---  1. Consider player size
---  2. Consider player speed (?)
---  3. Consider previous walls (?) (min delta between walls)
-generateRandomWalls :: Seed -> [Wall]
-generateRandomWalls s = _
+-- -- Infinite list of gameObj batches
+-- levelGenerator :: Seed -> [[GameObject]]
+-- levelGenerator s = _
 
--- Generate graph with walls as vertices and paths inbetween as edges
-makeGraph :: [Wall] -> Graph
-makeGraph walls = _
+-- -- Randomly generate walls
+-- --  1. Consider player size
+-- --  2. Consider player speed (?)
+-- --  3. Consider previous walls (?) (min delta between walls)
+-- generateRandomWalls :: Seed -> [GameObject]
+-- generateRandomWalls s = _
 
--- Get zone, represented by 2 Pgrams, from every point of which player could
--- reach the wall passed
-getInPaths :: Wall -> [Bounds]
-getInPaths w = _
+-- -- Generate graph with walls as vertices and paths inbetween as edges
+-- makeGraph :: [GameObject] -> Graph
+-- makeGraph walls = _
 
--- Get zone, represented by 2 Pgrams, which shows every point player could reach
--- from this wall
-getOutPaths :: Wall -> [Bounds]
-getOutpaths w = _
+-- -- Get zone, represented by 2 Pgrams, from every point of which player could
+-- -- reach the wall passed
+-- getInPaths :: GameObject -> [Bounds]
+-- getInPaths w = _
+
+-- -- Get zone, represented by 2 Pgrams, which shows every point player could reach
+-- -- from this wall
+-- getOutPaths :: GameObject -> [Bounds]
+-- getOutpaths w = _
 
 -- Get intersection of two trapezioidal bounds
+-- TODO change to intersectTrapBounds :: Bounds -> Bounds -> Maybe Bounds
 intersectTrapBounds :: Bounds -> Bounds -> Maybe Bounds
 intersectTrapBounds b b' = b''
     where
-        (x1, y1) = topLeft b
-        (x2, y2) = topRight b
-        (x3, y3) = bottomRight b
-        (x4, y4) = bottomLeft b
-        (x1', y1') = topLeft b'
-        (x2', y2') = topRight b'
-        (x3', y3') = bottomRight b'
-        (x4', y4') = bottomLeft b'
+        Point _ y1 = topLeft b
+        Point x2 y2 = topRight b
+        Point x3 _ = bottomRight b
+        Point _ y4 = bottomLeft b
+        Point x1' y1' = topLeft b'
+        Point x4' y4' = bottomLeft b'
         y_up = min y1 y1'
         y_low = max y4 y4'
         isCollinear = (y1 == y1') || (y4 == y4')
@@ -64,36 +64,35 @@ intersectTrapBounds b b' = b''
         y1'' = y_up
         x3'' = x1' - (x1' - x4')*(y1' - y_low)/(y1' - y4')
         y3'' = y_low
-        b'' = if isCollinear then Nothing else Just Bounds (Point x1'' y1'') (Point x2 y2) (Point x3'' y3'') (Point x4' y4') 
+        b'' = if isCollinear then Nothing else Just $ Bounds (Point x1'' y1'') (Point x2 y2) (Point x3'' y3'') (Point x4' y4') 
 
 -- Get player horizontal and vertical speed from distance passed
 calculateSpeed :: Double -> (Double, Double)
 calculateSpeed x = (horizontalSpeed, verticalSpeed)
-    whereg
+    where
         initialSpeed = 2    -- TODO: take from Core
         acceleration = 0.2  -- TODO: take from Core
         verticalSpeed = 4   -- TODO: take from Core
-        horizontalSpeed = maximum(tPath(acceleration / 2, initialSpeed, -x))
-        tPath (a, b, c) = [t1, t2]
+        horizontalSpeed = x / tPath(acceleration / 2, initialSpeed, -x)
+        tPath (a, b, c) = t1
             where
-                e = -b (2 * a)
+                e = -b * (2 * a)
                 d = b * b - 4 * a * c
-                t1 = e + sqrt d / (2 * a)
-			    t2 = e - sqrt d / (2 * a)
+                t1 = e + sqrt (d / (2 * a))
         
                 
--- Check whether player can reach second wall from first wall
-checkIntersection :: Wall -> Wall -> Bool
-checkIntersection w w' = _
+-- -- Check whether player can reach second wall from first wall
+-- checkIntersection :: GameObject -> GameObject -> Bool
+-- checkIntersection w w' = _
 
--- Check if graph contains feasible route from start to end
-checkGraph :: Graph -> Bool
-checkGraph g = _
+-- -- Check if graph contains feasible route from start to end
+-- checkGraph :: Graph -> Bool
+-- checkGraph g = _
 
--- Add spikes to reduce # of paths through graph or make path harder
-backwardGraphPass :: Graph -> [Spike]
-backwardGraphPass g = _
+-- -- Add spikes to reduce # of paths through graph or make path harder
+-- backwardGraphPass :: Graph -> [GameObject]
+-- backwardGraphPass g = _
 
--- Generate batch of gameObjects
-batchGenerator :: Seed -> [GameObject]
-batchGenerator s = _
+-- -- Generate batch of gameObjects
+-- batchGenerator :: Seed -> [GameObject]
+-- batchGenerator s = _
