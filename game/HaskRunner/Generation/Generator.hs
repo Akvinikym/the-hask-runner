ecd7e2c73c990d5bb44f3eaee2942c6932cec48b
olejeglejeg <oleg-taizov@yandex.ru>
2018-10-27 19:58:07 +0300

    + calculateSpeed function implemntation

--- game/HaskRunner/Generation/Generator.hs
+++ game/HaskRunner/Generation/Generator.hs
@@ -78,11 +78,22 @@ intersectTrapBounds b b' = b''
         b'' = Bounds (Point x1'' y1'') (Point x2'' y2'') (Point x3'' y3'') (Point x4'' y4'') 
 
 
-
 -- Get player horizontal and vertical speed from distance passed
-calculateSpped :: Double -> (Double, Double)
-calculateSpped x = _
-
+calculateSpeed :: Double -> (Double, Double)
+calculateSpeed x = (horizontalSpeed, verticalSpeed)
+    where
+        initialSpeed = 2    -- TODO: take from Core
+        acceleration = 0.2  -- TODO: take from Core
+        verticalSpeed = 4   -- TODO: take from Core
+        horizontalSpeed = maximum(tPath(acceleration / 2, initialSpeed, -x))
+        tPath (a, b, c) = [t1, t2]
+            where
+                e = -b (2 * a)
+                d = b * b - 4 * a * c
+                t1 = e + sqrt d / (2 * a)
+			    t2 = e - sqrt d / (2 * a)
+        
+                
 -- Check whether player can reach second wall from first wall
 checkIntersection :: Wall -> Wall -> Bool
 checkIntersection w w' = _
