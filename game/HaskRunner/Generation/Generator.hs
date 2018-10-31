ae2cde6187f28be2d4beb5c1f3afffe21aceb778
olejeglejeg <oleg-taizov@yandex.ru>
2018-10-31 22:02:27 +0300

    + path existence check

--- game/HaskRunner/Generation/Generator.hs
+++ game/HaskRunner/Generation/Generator.hs
@@ -1,12 +1,18 @@
 module HaskRunner.Generation.Generator where
 
 import qualified Data.Graph as G
+import Control.Monad
 import HaskRunner.Core
 import System.Random
 import Data.Maybe
 type Seed = Double
 
-
+-- | TODO: take from Core
+verticalSpeed = 4
+screenHeight = 10
+initialSpeed = 2    
+acceleration = 0.2 
+-- |
 
 -- Helper function for phi_inverse
 rational_approx :: Double -> Double
@@ -44,7 +50,7 @@ safeZone xOrigin = [ GameObject (Bounds
 
 -- Infinite list of gameObj batches
 levelGenerator :: Int -> [[GameObject]]
-levelGenerator s = scanl getNextXOrigin (safeZone 0.0) (map generateRandomWalls seedRvs)
+levelGenerator s = scanl getNextXOrigin (safeZone 0.0) (map getFeasibleRandomWalls seedRvs)
     where 
         g = mkStdGen s
         seedRvs =  (randoms g :: [Int])
@@ -62,6 +68,13 @@ makeWall x y l = GameObject bounds Platform
         p2 = Point (x + l) y
         p3 = Point (x + l) (y - platformHeight)
         p4 = Point x (y - platformHeight)
+
+
+getFeasibleRandomWalls :: Int -> Double -> [GameObject]
+getFeasibleRandomWalls s xOrigin = head $ dropWhile (not.isFeasible) (map (\s -> generateRandomWalls s xOrigin) seeds)
+        where
+            g = mkStdGen s
+            seeds =  (randoms g :: [Int])
 
 -- -- Randomly generate walls
 -- --  1. Consider player size (Done)
@@ -87,24 +100,31 @@ generateRandomWalls s xOrigin = zipWith3 makeWall platformXOrigins platformYOrig
 
 -- TODO insert this into generateRandomWalls
 isFeasible :: [GameObject] -> Bool
-isFeasible = True
+isFeasible gs = not (null gs) && G.path (makeGraph gs) 1 (length gs)
+
 
 -- Generate graph with walls as vertices and paths inbetween as edges
 makeGraph :: [GameObject] -> G.Graph
 makeGraph walls = G.buildG b edges
     where
         b = (1, (length walls))
-        inBounds = zip [1..] (map getInPaths walls)
-        outBounds = zip [1..] (map getOutPaths walls)
-        edges = catMaybes $ zipWith getEdges inBounds outBounds
+        inBounds = zip [1..] (map getIncomingTrapezoids walls)
+        outBounds = zip [1..] (map getOutcomingTrapezoids walls)
+        allBounds = unzip [(inB, outB) | inB <- inBounds, outB <- outBounds]
+        edges = catMaybes $ zipWith getEdges  (fst allBounds)  (snd allBounds)
+        
+        getEdges :: (Int, (Bounds, Bounds)) -> (Int, (Bounds, Bounds)) -> Maybe (Int, Int)
+        getEdges (i, (inBup, inBdown)) (j, (outBup, outBdown)) | edgeExists inBup inBdown outBup outBdown = Just (i, j)
+                                                               | otherwise = Nothing
 
-        getEdges :: (Int, Bounds) -> (Int, Bounds) -> Maybe (Int, Int)
-        getEdges (i, inB) (j, outB) | edgeExists inB outB = Just (i, j)
-                                    | otherwise = Nothing
+-- TODO add check for player sizes
+edgeExists :: Bounds -> Bounds -> Bounds -> Bounds -> Bool
+edgeExists inUp inDown outUp outDown = fromUpToDown || fromDownToUp
+    where
+    fromUpToDown = not $ isNothing (intersectTrapBounds inUp outDown) 
+    fromDownToUp = not $ isNothing (intersectTrapBounds inDown outUp) 
 
--- TODO add check for colliniearity
-edgeExists :: Bounds -> Bounds -> Bool
-edgeExists b b' = not (isNothing (intersectTrapBounds b b'))
+     
             
     
 -- -- Get zone, represented by 2 Pgrams, from every point of which player could
@@ -147,24 +167,51 @@ intersectTrapBounds b b' = b''
         y3'' = y_low
         b'' = if isCollinear then Nothing else Just $ Bounds (Point x1'' y1'') (Point x2 y2) (Point x3'' y3'') (Point x4' y4') 
 
--- Get player horizontal and vertical speed from distance passed
-calculateSpeed :: Double -> (Double, Double)
-calculateSpeed x = (horizontalSpeed, verticalSpeed)
+-- -- Get player horizontal and vertical speed from distance passed
+-- calculateSpeed :: Double -> (Double, Double)
+-- calculateSpeed x = (getHorizontalSpeed x, verticalSpeed)
+--     where
+--         verticalSpeed = 4   -- TODO: take from Core
+
+-- Get horizaontal speed based on player current position        
+getHorizontalSpeed :: Double -> Double
+getHorizontalSpeed x = horizontalSpeed
     where
-        initialSpeed = 2    -- TODO: take from Core
-        acceleration = 0.2  -- TODO: take from Core
-        verticalSpeed = 4   -- TODO: take from Core
         horizontalSpeed = x / timeFromX(acceleration / 2, initialSpeed, x)
         timeFromX (a, b, c) = t1
             where
                 e = -b * (2 * a)
                 d = b * b - 4 * a * (-c)
                 t1 = e + sqrt (d / (2 * a))
-        
-                
--- -- Check whether player can reach second wall from first wall
+
+-- Check whether player can reach second wall from first wall
 -- checkIntersection :: GameObject -> GameObject -> Bool
 -- checkIntersection w w' = _
+
+-- Obtain incoming trapezoids for wall
+getIncomingTrapezoids :: GameObject -> (Bounds, Bounds)
+getIncomingTrapezoids (GameObject bound _) = (getIncomingUpperBound (topLeft bound) (topRight bound), getIncomingLowerBound (bottomLeft bound) (bottomRight bound))
+    where
+        getIncomingUpperBound leftPoint rightPoint = Bounds (getIncomingUpperBoundPoint leftPoint) (getIncomingUpperBoundPoint rightPoint) rightPoint leftPoint
+        getIncomingUpperBoundPoint (Point x y) = (Point (calculateIncomingBoundXPoint x (screenHeight - y)) screenHeight)
+        getIncomingLowerBound leftPoint rightPoint = Bounds (getIncomingLowerBoundPoint leftPoint) (getIncomingLowerBoundPoint rightPoint) rightPoint leftPoint
+        getIncomingLowerBoundPoint (Point x y) = (Point (calculateIncomingBoundXPoint x y) 0)
+        calculateIncomingBoundXPoint x dY = x - getHorizontalSpeed (x - time) * time - 0.5 * acceleration * (time ^ 2)
+            where
+                time = dY / verticalSpeed
+
+-- Obtain outcoming trapezoids for wall
+getOutcomingTrapezoids :: GameObject -> (Bounds, Bounds)
+getOutcomingTrapezoids (GameObject bound _) = (getOutcomingUpperBound (topLeft bound) (topRight bound),  getOutcomingLowerBound (bottomLeft bound) (bottomRight bound))
+        where
+            getOutcomingUpperBound leftPoint rightPoint = Bounds (getOutcomingUpperBoundPoint leftPoint) (getOutcomingUpperBoundPoint rightPoint) rightPoint leftPoint
+            getOutcomingUpperBoundPoint (Point x y) = (Point (calculateOutcomingBoundXPoint x (screenHeight - y)) screenHeight)
+            getOutcomingLowerBound leftPoint rightPoint = Bounds leftPoint rightPoint (getOutcomingLowerBoundPoint rightPoint) (getOutcomingLowerBoundPoint leftPoint)
+            getOutcomingLowerBoundPoint (Point x y) = (Point (calculateOutcomingBoundXPoint x y) 0)
+            calculateOutcomingBoundXPoint x dY = x + getHorizontalSpeed x * time + 0.5 * acceleration * (time ^ 2)
+                where
+                    time = dY / verticalSpeed
+
 
 -- -- Check if graph contains feasible route from start to end
 -- checkGraph :: Graph -> Bool
