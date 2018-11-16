e30ccbd1fb4b5cec1be2fd28534fa9b6c7b47bb5
olejeglejeg <oleg-taizov@yandex.ru>
2018-11-16 15:41:16 +0300

    generator-refactoring
    + move platform height constant to constant block (on top of file)
    + add spaces
    - remove atavism functions
    -  remove excess declaration  of yLevels

--- game/HaskRunner/Generation/Generator.hs
+++ game/HaskRunner/Generation/Generator.hs
@@ -9,7 +9,6 @@ type Seed = Double
 
 -- | TODO: take from Core
 verticalSpeed = 4
--- screenHeight = 12
 initialSpeed = 2
 playerHeight = 2.0
 meanNumberOfWalls = 10
@@ -19,17 +18,17 @@ wallBase = 20.0
 meanWallLength = 1.0
 meanNumberOfSpikes :: Int
 meanNumberOfSpikes = 5
+platformHeight = 1.0
 -- |
 
 -- Helper function for phi_inverse
 rational_approx :: Double -> Double
-rational_approx t = t - ((0.010328*t + 0.802853)*t + 2.515517) /
-               (((0.001308*t + 0.189269)*t + 1.432788)*t + 1.0);
+rational_approx t = t - ((0.010328 * t + 0.802853) * t + 2.515517) / (((0.001308 * t + 0.189269) * t + 1.432788) * t + 1.0);
 
 -- Inverse of normal CDF
 phi_inverse :: Double -> Double
-phi_inverse p | p < 0.5 = - rational_approx( sqrt (-2.0*log(p)))
-              | otherwise = rational_approx( sqrt (-2.0*log(1.0 - p)))
+phi_inverse p | p < 0.5 = - rational_approx( sqrt (-2.0 * log(p)))
+              | otherwise = rational_approx( sqrt (-2.0 * log(1.0 - p)))
 
 -- {-|
 -- Generate infinite list if objects from random seed
@@ -60,19 +59,24 @@ safeZone xOrigin = [ GameObject (Bounds
 levelGenerator :: Int -> [[GameObject]]
 levelGenerator s = scanl getNextXOrigin (safeZone 0.0) makeObjects
     where
-        g = mkStdGen s
-        seedRvs =  (randoms g :: [Int])
-        randomWalls = map generateRandomWalls seedRvs
-        randomSpikes = map generateRandomSpikes seedRvs
-        -- [Double -> [GameObject]] [Double -> [GameObject]]
-        mergedObjects = zip randomSpikes randomWalls
+        -- makeObjects [Double -> [GameObject]] [Double -> [GameObject]]
         makeObjects = map (\ (x1, x2) t-> mergeWhile (x1 t) (x2 t)) mergedObjects
+            where
+                seedRvs =  randoms (mkStdGen s) :: [Int]
+                randomWalls = map generateRandomWalls seedRvs
+                randomSpikes = map generateRandomSpikes seedRvs
+                mergedObjects = zip randomSpikes randomWalls
         -- getNextXOrigin :: [GameObject] -> (Double -> [GameObject]) -> [GameObject]
         getNextXOrigin prev next =  (safeZone x) ++ [makeCoin (x+10.0) 0.0] ++ next (x + 10.0)
             where
                 Point x _ = topRight (bounds (last prev))
 
-platformHeight = 1.0
+-- Comparator returns true if first argument is larger than second
+mergeWhile :: Ord a => [a] -> [a] -> [a]
+mergeWhile [] xs = xs
+mergeWhile xs [] = xs
+mergeWhile (x:xs) (x' : xs') |  x < x' = [x] ++ mergeWhile xs ([x'] ++ xs')
+                                | otherwise = [x'] ++ mergeWhile ([x] ++ xs) xs'                
 
 makeWall :: Double -> Double -> Double -> GameObject
 makeWall x y l = GameObject bounds Platform
@@ -83,18 +87,10 @@ makeWall x y l = GameObject bounds Platform
         p3 = Point (x + l) (y - platformHeight)
         p4 = Point x (y - platformHeight)
 
--- Comparator returns true if first argument is larger than second
-mergeWhile :: Ord a => [a] -> [a] -> [a]
-mergeWhile [] xs = xs
-mergeWhile xs [] = xs
-mergeWhile (x:xs) (x' : xs') |  x < x' = [x] ++ mergeWhile xs ([x'] ++ xs')
-                             | otherwise = [x'] ++ mergeWhile ([x] ++ xs) xs'
-
 getFeasibleRandomWalls :: Int -> Double -> [GameObject]
 getFeasibleRandomWalls s xOrigin = head $ dropWhile (not.isFeasible) (map (\s -> generateRandomWalls s xOrigin) seeds)
         where
-            g = mkStdGen s
-            seeds =  (randoms g :: [Int])
+            seeds =  (randoms (mkStdGen s) :: [Int])
 
 -- -- Randomly generate walls
 -- --  1. Consider player size (Done)
@@ -103,13 +99,12 @@ getFeasibleRandomWalls s xOrigin = head $ dropWhile (not.isFeasible) (map (\s ->
 generateRandomWalls :: Int -> Double -> [GameObject]
 generateRandomWalls s xOrigin = zipWith3 makeWall platformXOrigins platformYOrigins platformLenghts
         where
-            yLevels :: Int
             yLevels = 6
             normalRvs = map phi_inverse (randomRs (0.0, 1.0) (mkStdGen s))
             uniformRvs = randomRs (0, yLevels) (mkStdGen s)
             numberOfWalls = round (meanNumberOfWalls * (head normalRvs))
             platformLenghts = map (\x -> x * meanWallLength + wallBase) (take numberOfWalls (drop 1 normalRvs))
-            platformYOrigins = map (\x -> ((screenHeight*2) / (fromIntegral yLevels)) * (fromIntegral x) - screenHeight) (take numberOfWalls uniformRvs)
+            platformYOrigins = map (\x -> ((screenHeight * 2) / yLevels) * x - screenHeight) (take numberOfWalls uniformRvs)
             platformXOrigins = scanl (+) xOrigin (map (\x -> baseOriginOffset + meanOriginOffset * x) (take numberOfWalls (drop (1 + numberOfWalls) normalRvs)))
 
 
@@ -135,12 +130,11 @@ makeCoin x y = GameObject bounds Coin
 generateRandomSpikes :: Int -> Double -> [GameObject]
 generateRandomSpikes s xOrigin = zipWith makeSpike platformXOrigins platformYOrigins
         where
-            yLevels :: Int
             yLevels = 6
             uniformRvs = randomRs (0, yLevels - 1) (mkStdGen s)
             numberOfspikes = meanNumberOfSpikes
             xPositions = map (6.32 * ) [0, 1/ (fromIntegral numberOfspikes) .. 1]
-            platformYOrigins = map (\x -> ((screenHeight*2) / (fromIntegral yLevels)) * (fromIntegral x) - screenHeight + 1.5) (take numberOfspikes uniformRvs)
+            platformYOrigins = map (\x -> ((screenHeight * 2) / yLevels) * x - screenHeight + 1.5) (take numberOfspikes uniformRvs)
             platformXOrigins = map (xOrigin +)  xPositions
 
 
@@ -187,17 +181,11 @@ intersectTrapBounds b b' = b''
         y_up = min y1 y1'
         y_low = max y4 y4'
         isCollinear = (y1 == y1') || (y4 == y4')
-        x1'' = (y_up - y4)*(x2 - x3)/(y2 - y4) + x3
+        x1'' = (y_up - y4) * (x2 - x3) / (y2 - y4) + x3
         y1'' = y_up
-        x3'' = x1' - (x1' - x4')*(y1' - y_low)/(y1' - y4')
+        x3'' = x1' - (x1' - x4') * (y1' - y_low) / (y1' - y4')
         y3'' = y_low
         b'' = if isCollinear then Nothing else Just $ Bounds (Point x1'' y1'') (Point x2 y2) (Point x3'' y3'') (Point x4' y4')
-
--- -- Get player horizontal and vertical speed from distance passed
--- calculateSpeed :: Double -> (Double, Double)
--- calculateSpeed x = (getHorizontalSpeed x, verticalSpeed)
---     where
---         verticalSpeed = 4   -- TODO: take from Core
 
 -- Get horizaontal speed based on player current position
 getHorizontalSpeed :: Double -> Double
@@ -207,13 +195,14 @@ getHorizontalSpeed x = horizontalSpeed
         timeFromX (a, b, c) = t1
             where
                 e = -b * (2 * a)
-                d = b * b - 4 * a * (-c)
+                d = b ^ 2 - 4 * a * (-c)
                 t1 = e + sqrt (d / (2 * a))
 
 -- Check whether player can reach second wall from first wall
 -- checkIntersection :: GameObject -> GameObject -> Bool
 -- checkIntersection w w' = _
 
+-- TODO: rewrite these two functions on polymorphic functions
 -- Obtain incoming trapezoids for wall
 getIncomingTrapezoids :: GameObject -> (Bounds, Bounds)
 getIncomingTrapezoids (GameObject bound _) = (getIncomingUpperBound (topLeft bound) (topRight bound), getIncomingLowerBound (bottomLeft bound) (bottomRight bound))
