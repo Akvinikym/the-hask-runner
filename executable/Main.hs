3c56602a1417d64f6af5e41bf06144cddc8e410a
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-11-24 00:41:27 +0300

    add score server

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified GlossRunner
+import Network.HTTP.Simple
+import qualified Data.ByteString.Char8 as B8
 
 main :: IO ()
 main = GlossRunner.mainLoop
+
+-- main :: IO ()
+-- main = httpBS "http://example.com" >>= B8.putStrLn . getResponseBody
